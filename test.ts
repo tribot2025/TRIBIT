7942a7c4dc25faee8e9a780f8ccf3e1582a6dbda
tribot2025 <tribot.2025@gmail.com>
2026-04-20 16:33:08 +0700

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,11 +1,11 @@
 // tests go here; this will not be compiled when this package is used as an extension.
-OKBit.ADC.AnalogInitial();
-OKBit.GPIO.GpioInit();
-OKBit.PWM.PWMinit();
-OKBit.OLED.init();
+TRIBIT.ADC.AnalogInitial();
+TRIBIT.GPIO.GpioInit();
+TRIBIT.PWM.PWMinit();
+TRIBIT.OLED.init();
 
-OKBit.PWM.SetMotorSpeedRaw(0, 500, OKBit.PWM.MotorDirection.Forward)
-OKBit.PWM.SetMotorSpeedRaw(1, 500, OKBit.PWM.MotorDirection.Backward)
+TRIBIT.PWM.SetMotorSpeedRaw(0, 500, TRIBIT.PWM.MotorDirection.Forward)
+TRIBIT.PWM.SetMotorSpeedRaw(1, 500, TRIBIT.PWM.MotorDirection.Backward)
 
 
 basic.forever(function() {
@@ -19,7 +19,7 @@ let adc_timestamp_ms = 0;
 function adc_update(): void{
     if(control.millis() - adc_timestamp_ms >= 1000){
         for(let i = 0; i < 8 ; i++){
-            OKBit.OLED.writeNumNewLine(OKBit.ADC.AnalogReadVoltage(i));
+            TRIBIT.OLED.writeNumNewLine(TRIBIT.ADC.AnalogReadVoltage(i));
         }
         adc_timestamp_ms = control.millis();
     }
@@ -29,7 +29,7 @@ let gpio_timestamp_ms = 0;
 let gpio_cnt = 0;
 function gpio_update(): void{
     if(control.millis() - gpio_timestamp_ms >= 100){
-        OKBit.GPIO.GpioWrite(gpio_cnt, OKBit.GPIO.GpioRead(gpio_cnt) ? 0 : 1);
+        TRIBIT.GPIO.GpioWrite(gpio_cnt, TRIBIT.GPIO.GpioRead(gpio_cnt) ? 0 : 1);
         gpio_cnt = (gpio_cnt+1)% 16;
         gpio_timestamp_ms = control.millis();
     }
@@ -41,7 +41,7 @@ let servo_dir = 0;
 function pwm_update(): void{
     if(control.millis() - pwm_timestamp_ms >= 1){
         for(let i = 0; i < 8; i++){
-            OKBit.PWM.SetServoAngle(i,servo_angle);
+            TRIBIT.PWM.SetServoAngle(i,servo_angle);
         }
         servo_angle += servo_dir ? -1:1 ;
 
